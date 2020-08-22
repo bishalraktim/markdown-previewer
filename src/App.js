fa34e813e0299b23e4d206e5f1cde12fc455c7c7
bishalraktim <raktimbishal@gmail.com>
2020-08-22 20:47:01 +1000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Quotation from './components/Quotation/Quotation';
 
 class App extends Component {
+
   render () {
     return (
       <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
+        <Quotation />
       </div>
     );
   }
